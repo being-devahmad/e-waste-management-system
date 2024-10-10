9702ccc57dffb3e08a4aecee241d142df995d3ee
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-10-11 00:59:39 +0500

    update schema.ts

--- src/utils/db/schema.ts
+++ src/utils/db/schema.ts
@@ -1,6 +1,7 @@
 
 import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 
+// User model
 export const Users = pgTable("users", {
     id: serial('id').primaryKey(),
     email: varchar('email', { length: 255 }).notNull().unique(),
