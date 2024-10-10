2580364e4d65449e06c3b365fa0a4ae48247a88d
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-10-11 00:56:58 +0500

    Initial commit

---
+++ src/utils/db/schema.ts
@@ -0,0 +1,9 @@
+
+import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const Users = pgTable("users", {
+    id: serial('id').primaryKey(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+    name: varchar('name', { length: 255 }).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
+})
