e8c769ba7c4552956d0fc3511e121993d32381f5
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-10-11 10:35:13 +0500

    rewards and reports model commit

--- src/utils/db/schema.ts
+++ src/utils/db/schema.ts
@@ -1,10 +1,37 @@
 
-import { pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, jsonb, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
-// User model
+// Users model
 export const Users = pgTable("users", {
     id: serial('id').primaryKey(),
     email: varchar('email', { length: 255 }).notNull().unique(),
     name: varchar('name', { length: 255 }).notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull()
+})
+
+// Reports model
+export const Reports = pgTable("reports", {
+    id: serial('id').primaryKey(),
+    userId: integer("user_id").references(() => Users.id).notNull(), // in reference with the users table 
+    location: text('locatin').notNull(),
+    wasteType: varchar('waste_type', { length: 255 }).notNull(),
+    amount: varchar('amount', { length: 255 }).notNull(),
+    imageUrl: text('image_url'),
+    verificationResult: jsonb('verification_result'),
+    status: varchar('status', { length: 255 }).notNull().default('pending'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    collectorId: integer('collector_id').references(() => Users.id).notNull() // in reference with the users table 
+})
+
+// Rewards model
+export const Rewards = pgTable('reports', {
+    id: serial('id').primaryKey(),
+    userId: integer("user_id").references(() => Users.id).notNull(), // in reference with the users table 
+    points: integer('points').notNull().default(0),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    isAvailable: boolean('is_available').notNull().default(true),
+    description: text('description'),
+    name: varchar('name', { length: 255 }).notNull(),
+    collectionInfo: text('collection_info').notNull()
 })
