7d868f1a3536b6b9868772784e3b18acbc0dbf25
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-10-11 10:48:10 +0500

    Update schema.ts

--- src/utils/db/schema.ts
+++ src/utils/db/schema.ts
@@ -35,3 +35,32 @@ export const Rewards = pgTable('reports', {
     name: varchar('name', { length: 255 }).notNull(),
     collectionInfo: text('collection_info').notNull()
 })
+
+// CollectedWaste model
+export const CollectedWaste = pgTable('collected_waste', {
+    id: serial('id').primaryKey(),
+    reportId: integer('report_id').references(() => Reports.id).notNull(), // in reference with the reports table 
+    collectorId: integer('collector_id').references(() => Users.id).notNull(), // in reference with the users table 
+    collectionDate: timestamp('created_at').defaultNow().notNull(),
+    status: varchar('status', { length: 255 }).notNull().default('collected'),
+})
+
+// Notification model
+export const Notifications = pgTable('notifications', {
+    id: serial('id').primaryKey(),
+    userId: integer("user_id").references(() => Users.id).notNull(), // in reference with the users table 
+    message: text('message'),
+    type: varchar('type', { length: 50 }).notNull(),
+    isRead: boolean('is_read').notNull().default(false),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+})
+
+// Transactions model
+export const Transactions = pgTable('transactions', {
+    id: serial('id').primaryKey(),
+    userId: integer("user_id").references(() => Users.id).notNull(), // in reference with the users table 
+    type: varchar('type', { length: 20 }).notNull(),
+    amount: integer('amount').notNull(),
+    description: text('description').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+})
