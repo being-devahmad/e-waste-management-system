bd633d7e8dad153a7cd340c4567cfff5d8737209
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-10-20 20:11:38 +0500

    gemini api integrated and reports page updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
     env: {
         DB_URL: process.env.DB_URL,
-        WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID
+        WEB3_AUTH_CLIENT_ID: process.env.WEB3_AUTH_CLIENT_ID,
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+        GEMINI_API_KEY: process.env.GEMINI_API_KEY
     }
 };
 
--- utils/db/actions.ts
+++ utils/db/actions.ts
@@ -1,5 +1,5 @@
 import { db } from './dbConfig'
-import { Notifications, Transactions, Users } from './schema'
+import { Notifications, Reports, Rewards, Transactions, Users } from './schema'
 import { eq, sql, and, desc } from 'drizzle-orm'
 
 
@@ -93,6 +93,122 @@ export async function markNotificationAsRead(notificationId: number) {
 
     } catch (error) {
         console.log('Error marking notification as read', error)
+        return null
+    }
+}
+
+
+export async function createReport(
+    userId: number,
+    location: string,
+    wasteType: string,
+    amount: string,
+    imageUrl?: string,
+    verificationResult?: any
+) {
+    try {
+        const [report] = await db
+            .insert(Reports)
+            .values({
+                userId,
+                location,
+                wasteType,
+                amount,
+                imageUrl,
+                verificationResult,
+                status: 'pending',
+            })
+            .returning()
+            .execute()
+
+        return report
+
+        const pointsEarned = 10;
+        // updateRewardPoints
+        await updateRewardPoints(userId, pointsEarned)
+        // createTransaction
+        await createTransaction(
+            userId,
+            'earned_report',
+            pointsEarned,
+            'Points earned for reporting waste'
+        )
+        // createNotification
+        await createNotification(
+            userId,
+            `You have earned ${pointsEarned} points for reporting waste`,
+            "reward"
+        )
+        return report
+    } catch (error) {
+        console.error("Error creating report", error)
+    }
+
+}
+
+
+export async function updateRewardPoints(userId: number, pointsToAdd: number) {
+    try {
+        const [updateReward] = await db
+            .update(Rewards)
+            .set({
+                points: sql`${Rewards.points} + ${pointsToAdd}`
+            })
+            .where(eq(Rewards.userId, userId))
+            .returning()
+            .execute()
+    } catch (error) {
+        console.error('Error updating reward points', error)
+        return null
+    }
+}
+
+export async function createTransaction(
+    userId: number,
+    type: 'earned_report' | 'earned_collect' | 'redeemed',
+    amount: number,
+    description: string
+) {
+    try {
+        const [transaction] = await db
+            .insert(Transactions)
+            .values({
+                userId,
+                type,
+                amount,
+                description
+            })
+            .returning()
+            .execute()
+
+        return transaction
+
+    } catch (error) {
+        console.error('Error creating transaction', error)
+        return null
+    }
+}
+
+
+export async function createNotification(
+    userId: number,
+    message: string,
+    type: string,
+) {
+    try {
+        const [notification] = await db
+            .insert(Notifications)
+            .values({
+                userId,
+                message,
+                type
+            })
+            .returning()
+            .execute()
+
+        return notification
+    } catch (error) {
+        console.error('Error creating notification', error)
         return null
     }
 }
